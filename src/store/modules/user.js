fdfff2ab517df398bda915013a0bd0641e9fb9ce
cutiechi <chi@cutie.ooo>
2018-09-26 13:28:15 +0800

    feat(vuex): add list all users action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,14 @@
+import { listAll } from '../../api/user'
+
 const state = {
   users: []
 }
 
 const actions = {
-
+  async listAll ({ commit }) {
+    const { data: users } = await listAll()
+    commit('SET_USERS', users)
+  }
 }
 
 const mutations = {
