5a5b9ef86bcd12eac7379c502285e5e29da6cace
cutiechi <chi@cutie.ooo>
2018-09-27 16:38:48 +0800

    feat(page): add user page insert and list all component

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,3 +1,20 @@
 <template>
-  <div id="user"></div>
+  <div id="user">
+    <el-card>
+      <insert></insert>
+      <list-all></list-all>
+    </el-card>
+  </div>
 </template>
+
+<script>
+import ListAll from '../components/User/ListAll'
+import Insert from '../components/User/Insert'
+
+export default {
+  components: {
+    ListAll,
+    Insert
+  }
+}
+</script>
